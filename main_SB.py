d8b51dd088e25c5da9c5065b7e66ebcd62ed6027
ayogggua <34470308+ayogggua@users.noreply.github.com>
2019-05-22 22:20:43 +0800

    E:\pyTraining
    傻逼第一天

---
+++ main_SB.py
@@ -0,0 +1,11 @@
+class IDM1_SB(object):
+
+
+	def __init__(self, name, gender, department, S_level, SB_level):
+		self.name = name
+		self.gender = gender
+		self.department = department
+		self.S_level = S_level
+		self.SB_level = SB_level
+	def information(self):
+		print('name:s%,gender:%s,department:%s,S_level:%s,SB_level:%s'%(self.name,self.gender,self.department,self.S_level,self.SB_level))
