b2575140de57ddc8f688b66a7941a75cfad42276
ayogggua <34470308+ayogggua@users.noreply.github.com>
2019-06-04 17:35:02 +0800

    验证码生成器

--- v_code.py
+++ v_code.py
@@ -1,16 +1,34 @@
 import random
+import time
 def vcode(chart_len = 4):
+    global v_code
     v_chart = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    print v_chart
+    # print v_chart
     v_chart_list = list(v_chart)
     v_len = len(v_chart)
     v_code_list = []
-    print(v_chart_list)
+    # print(v_chart_list)
     for i in range(chart_len):
         index = random.randint(0,v_len - 1)
         print(index)
         v_code_list.append(v_chart_list[index])
     v_code = ''.join(v_code_list)
     print(v_code)
+    return v_code
 
-vcode()
+def trigger():
+    zhongjianghaoma = 'aJ3f'
+    count = 0
+    while True:
+        time.sleep(0.5)
+        count += 1
+        print ('%s times'%count)
+        vcode()
+        zhongjianghaoma = 'aJ3f'
+        if zhongjianghaoma == v_code:
+          print('zhongjiangle')
+          break
+        else:
+          continue
+if __name__ == '__main__':
+    trigger()
